2fc1c3910206b54839f473530cd8ca06e10866ae
AnSuHan <glorygem195@gmail.com>
2024-05-28 15:31:57 +0900

    기본 route 설정

--- nodejs_aws/routes/index.js
+++ nodejs_aws/routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/api/get/api-get', function(req, res){
+	res.status(200).json({
+    	"message" : "get message from nodejs-api get"
+        });
+ });
+
+ router.post('/api/post/api-post',function(req, res){
+	res.status(200).json({
+    	"message" : "get message from nodejs-api post"
+    });    
+});  
+
 module.exports = router;
